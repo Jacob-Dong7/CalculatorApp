ad1d4a8bf2e6b76256ed6091198b9d90df1b448e
JDOT <JDON219206@gmail.com>
2025-11-25 18:37:49 +1100

    feat: Added multiplication feature

--- calculate.cpp
+++ calculate.cpp
@@ -3,12 +3,27 @@
 #include <iostream>
 
 double Calculate::calculate(std::vector<double> list) {
-    bool solved = 0;
+    bool multAll = 0;
     total = list[0];
+    int mult;
+    int divide;
 
+    while (multAll == 0) {
+        for (size_t i = 0; i < list.size(); ++i) {
+            if (list[i] == -97) {
+                mult = list[i - 1] * list[i + 1];
+                list.erase(list.begin() + i - 1, list.begin() + i + 2);
+                list.insert(list.begin() + i - 1, mult);
+                multAll = 0;
+                break;
+            } else {
+                multAll = 1;
+            }
+        }
 
+    }
+    for (size_t i = 0; i < list.size(); ++i) std::cout << list[i] << std::endl;
     for (size_t i = 1; i < list.size(); ++i) {
-        std::cout << list[i] << std::endl;
         if (list[i] == -99) total += list[i + 1];
         if (list[i] == -98) total -= list[i + 1];
 
